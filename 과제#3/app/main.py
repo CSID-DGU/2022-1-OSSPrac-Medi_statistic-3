6b8a5c040e33f68c156f798b03e80379412bcbb6
Jongbeom Park <101785678+jongbeom1130@users.noreply.github.com>
2022-05-26 14:50:27 +0900

    Add Programming Languages

--- 과제#3/app/main.py
+++ 과제#3/app/main.py
@@ -14,8 +14,8 @@ def result():
       result['Student number'] = request.form.get('Student number')
       # 학과
       result['Major'] = request.form.get('Major')
-      # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
-
+      # 프로그래밍 언어
+      result['Programming Languages'] = ', '.join(request.form.getlist("Programming Languages"))
       return render_template("result.html",result = result)
 
 if __name__ == '__main__':
