fd1a95da6baad167ee1b2f7eee6cbd5429143f3f
Yonghwi Lee <101785899+yooong@users.noreply.github.com>
2022-06-02 13:21:07 +0900

    Add files via upload

---
+++ HW#4/app/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')  # default URL
+def student():
+   return render_template('input_info.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = dict()
+      result['Name'] = request.form.get('Name')
+
+      # 학번
+      result['Student number'] = request.form.get('Student number')
+      # 학과
+      result['Major'] = request.form.get('Major')
+      # 프로그래밍 언어
+      result['Programming Languages'] = ', '.join(request.form.getlist("Programming Languages"))
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run()
